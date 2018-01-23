8a890083a9be716f3a1d1e5e91f1c983b3183729
Igo Medeiros <igomedeiros@MacBook-Pro-de-Igo.local>
2018-01-23 10:55:23 -0200

    add tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,3 +1,46 @@
-describe('main', function(){
-  
+var expect = require('chai').expect;
+
+describe('main', function() {
+  var arr;
+  before(function() {
+
+  });
+
+  after(function() {
+
+  });
+
+  beforeEach(function() {
+    arr = [1,2,3];
+  });
+
+  afterEach(function() {
+
+  });
+
+  it('should be an array', function(){
+    expect(arr).to.be.a('array');
+  });
+
+
+  it('should have a size of 4 when push another value to array', function() {
+    arr.push(arr);
+    expect(arr).to.have.lengthOf(4)
+  });
+
+  it('schould remove the value 3 when user pop in the array', function(){
+    arr.pop();
+    expect(arr).to.not.include(3);
+  });
+
+  it('should return true when pop 3 from the array', function(){
+    expect(arr.pop() === 3).to.be.true;
+  });
+
+
+  it('schould have a sizer of 2 pop a value from the array', function() {
+    arr.pop();
+    expect(arr).to.have.lengthOf(2);
+  });
+
 })
